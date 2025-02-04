96b0153cb8282b3895b8909f89a112967652e490
matheus2306 <math2306.r@gmail.com>
2025-02-04 14:06:43 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPI.Models;
+
+namespace PrimeiraAPI.Controllers
+{
+    //Nome da rota para acessar o controller
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("primes", Name = "GetPrimes")]
+        public IEnumerable<int> GetPrimes()
+        {
+            return Enumerable.Range(0, 1001).Where(IsPrime).ToList();
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
